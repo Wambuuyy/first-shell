733cb4abb702f0e3762fdf55539616b3126caf70
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-11-07 16:09:00 +0300

    started the main loop

---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the simple shell
+ * @argc: argument count (no of input arg)
+ * @argv: argument vector (array of input arg)
+ * @env: Array of environment variables
+ * Return: 0 on success
+ */
+
+int  main(int argc, char **argv, char **env 
