827f6613ab42af758c7e330c96a68809668a0a44
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-11-08 04:25:41 +0300

    add srtcpy_ function

---
+++ special.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+ * strlen_ - calculates the length of the string
+ * @string: string whose length is being determined
+ * Return: the length of the string
+ */
+size_t strlen_(const char *string)
+{
+  size_t len = 0;
+
+  while (string[len] != '\0')
+    len++;
+  return len
+}
+
+/**
+ * strcpy_ - copies a string to a different string
+ * @source: pointer to the string that is being copied
+ * @destination: the pointer to where the string will be copied to
+ * Return: pointer to the copied string
+ */
+
+char *strcpy_(char *destination, const char *source)
+{
+    char *start = destination;
+    size_t i = 0;
+
+    while (src[i] != '\0') {
+        destination[i] = src[i];
+        i++;
+    }
+    destination[i] = '\0'; // Append null character at the end
+    return start;
+}
