6f143407f2b39b0ddeb257b616f3d392f3d75588
Prudence Wambui <wambuiprudy@gmail.com>
2023-11-08 19:14:14 +0300

    remove betty errors for print file but still needs description

--- print.c
+++ print.c
@@ -1,22 +1,20 @@
 #include "shell.h"
 
-#include <unistd.h>
-
 void perror_(const char *program, int err_code, char **args)
 {
-  char *error_message = "!!Error";
-  char *colon = ": ";
-  char *status = ": not found";
-  char * err_code = itoa_(err_code);
+	char *error_message = "!!Error";
+	char *colon = ": ";
+	char *status = ": not found";
+	char *err_code = itoa_(err_code);
 
-  write(STDERR_FILENO, program, strlen_(program)); // Replace 5 with the appropriate length of the program name
-  write(STDERR_FILENO, colon, 2);
-  write(STDERR_FILENO, error_message, strlen_(error_message)); // Replace 5 with the appropriate length of the error message
-  write(STDERR_FILENO, " ", 1);
-  write(STDERR_FILENO, err_code, strlen(err_code));
-  free(err_code);
-  write(STDERR_FILENO, colon, strlen(colon));
-  write(STDERR_FILENO, args[0], strlen_(args[0])); // Adjust the index and size as needed for the command list argument
-  write(STDERR_FILENO, not_found, strlen(not_found));
-  write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, program, strlen_(program));
+	write(STDERR_FILENO, colon, 2);
+	write(STDERR_FILENO, error_message, strlen_(error_message));
+	write(STDERR_FILENO, " ", 1);
+	write(STDERR_FILENO, err_code, strlen(err_code));
+	free(err_code);
+	write(STDERR_FILENO, colon, strlen(colon));
+	write(STDERR_FILENO, args[0], strlen_(args[0]));
+	write(STDERR_FILENO, not_found, strlen(not_found));
+	write(STDERR_FILENO, "\n", 1);
 }
