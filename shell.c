960c9a764c42dfea68416d615fe92e804440a69f
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-11-08 05:50:42 +0300

    completed the function but not yet done

--- shell.c
+++ shell.c
@@ -11,23 +11,37 @@
 int  main(int __attribute__((unused)) argc, char **argv, char **env)
 {
   char *input_buf = NULL;
-  size_t buf_size = BUFFER_SIZE;
+  char ** tokens = NULL;
   ssize_t chars_number = 0;
   int iterations = 0;
 
   while (1)
-    {
+  {
       iterations++;
       display_prompt();
       signal_handler();
-      chars_number = getline(&input_buf, &bufsize, stdin);
+      chars_number = getline(&input_buf, &BUFFER_SIZE, stdin);
       if (chars_number == -1)/* EOF is rep by -1*/
         handle_EOF(input_buf);
       else if (*input_buf == '\n')
         free(input_buf);
       else
       {
-        /* Im going to create a function to remove a new line, strlen, a tokenizer,
-        
-  
+        rem_newline(input_buf);
+        tokens = tokenizer(input_buf, " \0");
+        free(input_buf);
+        if (!is_exit_builtin(tokens[0]))
+          exit_shell(tokens, av[0], env, iterations);
+        else if (!is_cd_builtin(tokens[0]))
+          execute_cd_command(tokens);
+        else
+          create_child_process(tokens, av[0], env, iterations);
+      }
+      clear_stdin_buffer();/*fflush(stdin)*/
+      input_buf = NULL;
+      BUFFER_SIZE = 0;/* reset to 0/
+  }
+  if (chars_number == -1)
+    return (EXIT_FAILURE);
+  return(EXIT_SUCCESS);
 }
