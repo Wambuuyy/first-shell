9505dcbca8d6d90b2b0a8bf6dabe952547ddae1d
Prudence Wambui <wambuiprudy@gmail.com>
2023-11-08 19:09:22 +0300

    remove betty errors from pid.c

--- pid.c
+++ pid.c
@@ -10,35 +10,36 @@
  */
 void child(char **args, char *program, char **env, int iterations)
 {
-  pid_t pid;
-  int status = 0, error;
+	pid_t pid;
+	int status = 0, error;
 
-  pid = fork();
-    
-    if (pid < 0)
-    {
-        perror("fork");
-        exit_free(args);
-        exit(EXIT_FAILURE);
-    }
-    if (pid == 0)
-    {
-        if (execve(args[0], args, env) == -1)
-        {
-            perror("execve");
-            exit_free(args);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-      /*parent process*/
-      while (wait(&status) != pid)
-      {
-        /*it waits for the childprocess to end*/
-        continue;
-      }
-      error = waitpid(pid, &status, 0);
-      (error < 0)? perror("waitpid"): exit_free(args);
-    }
+	pid = fork();
+
+
+	if (pid < 0)
+	{
+		perror("fork");
+		exit_free(args);
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if (execve(args[0], args, env) == -1)
+		{
+			perror("execve");
+			exit_free(args);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/*parent process*/
+		while (wait(&status) != pid)
+		{
+			/*it waits for the childprocess to end*/
+			continue;
+		}
+		error = waitpid(pid, &status, 0);
+		(error < 0) ? perror("waitpid") : exit_free(args);
+	}
 }
