65feb0789285dd13a2993ca4bd94be4d58ef3b23
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-11-08 06:06:10 +0300

    added strcmp_function

--- special.c
+++ special.c
@@ -33,3 +33,26 @@ char *strcpy_(char *destination, const char *source)
     destination[i] = '\0'; // Append null character at the end
     return start;
 }
+
+/**
+ * strcmp_ - Compares two strings.
+ * @string1: The first input string.
+ * @string2: The second input string.
+ *
+ * Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+ *         and a positive value if s1 is greater than s2.
+ */
+int strcmp_(const char *string1, const char *string2) {
+    while (*string1 && *string2)
+    {
+      if(*string1 != *string2)
+      {
+        return *string1 - *string2;
+      }
+      string1++;
+      string2++;
+    }
+
+    return *string1 - *string2;
+}
+
