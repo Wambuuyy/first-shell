f1e7fc3df4e4f81b884c8554f351541ed6809d83
Prudence Wambui <wambuiprudy@gmail.com>
2023-11-08 19:03:25 +0300

    remove betty errors on file exit.c

--- exit.c
+++ exit.c
@@ -1,17 +1,13 @@
 #include "shell.h"
 
 /**
- * exit_builtin - a function that checks if a string is the exit built-in command.
+ * exit_builtin - checks if a string is the exit built-in command.
  * @string: The command to check if it is exit
- *
  * Return: 1 if the command is the exit built-in command, 0 otherwise.
  */
-int exit_builtin(char *string) {
-    if (strcmp_(command, "exit") == 0) {
-        return 1;
-    } else {
-        return 0;
-    }
+int exit_builtin(char *string)
+{
+	(strcmp_(command, "exit") == 0) ? return : return 0;
 }
 
 /**
@@ -24,44 +20,44 @@ int exit_builtin(char *string) {
  */
 void exit_shell(char **args, char *input_buf, char *program, int exit_code)
 {
-    int condition = 0, i;
+	int condition = 0, i;
 
-    if (args[1] == NULL)
-    {
-        free(input_buf);
-        free_str(args);
-        exit(EXIT_SUCCESS);
-    }
-    if(err_number == 2)/* the ENOENT code*/
-    {
-        free(input_buf);
-        free_str(args);
-        pru_exit(2);
-    }
-    for (i = 0; args[1][i] != '\0'); i++)
-    {
-        if (isalpha(args[1][i] != 0)
-        {
-            perror_(program, err_code, args);
-            break;
-        }
-        else
-        {
-            condition = atoi(args[1]);
-            if (condition < 0)
-            {
-                free(input_buf);
-                free_str(args);
-                exit(2);
-            }
-            else
-            {
-                free(input_buf);
-                free_str(args);
-                exit(condition & 0XFF);
-            }
-        }
-    }
+	if (args[1] == NULL)
+	{
+		free(input_buf);
+		free_str(args);
+		exit(EXIT_SUCCESS);
+	}
+	if (err_number == 2)/* the ENOENT code*/
+	{
+		free(input_buf);
+		free_str(args);
+		pru_exit(2);
+	}
+	for (i = 0; (args[1][i] != '\0'); i++)
+	{
+		if (isalpha(args[1][i] != 0)
+		{
+			perror_(program, err_code, args);
+			break;
+		}
+		else
+			{
+			condition = atoi(args[1]);
+			if (condition < 0)
+			{
+				free(input_buf);
+				free_str(args);
+				exit(2)
+			}
+			else
+			{
+				free(input_buf);
+				free_str(args);
+				exit(condition & 0XFF);
+			}
+		}
+	}
 }
 
 /*
@@ -71,12 +67,12 @@ void exit_shell(char **args, char *input_buf, char *program, int exit_code)
  */
 void exit_free(char **args)
 {
-    size_t i;
+	size_t i;
 
-    if (args ==NULL)
-        return;
-    for (i = 0; args[i]; i++)
-        free(args[i]);
-    (args[i] == NULL)? free(args[i]): free(args);
-    exit(EXIT_FAILURE);
+	if (args == NULL)
+		return;
+	for (i = 0; args[i]; i++)
+		free(args[i]);
+	(args[i] == NULL) ? free(args[i]) : free(args);
+	exit(EXIT_FAILURE);
 }
