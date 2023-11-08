b68d1973f46db48e60e5a5d9205fabdbde9b8716
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-11-08 15:38:07 +0300

    had lost my function

--- change_dir.c
+++ change_dir.c
@@ -1,26 +1,45 @@
 #include "shell.h"
 
 /**
- * is_cd_builtin - checks if a command is a cd builtin
- * @command: The command to check
+ * cd_builtin - checks if a command is a cd builtin
+ * @args: The command to check
  * Return: 1 if command is cd, 0 otherwise
  */
-int is_cd_builtin(char *command) {
-  if (_strcmp(command, "cd") == 0)
+int cd_builtin(char *args) {
+  if (strcmp_(args, "cd") == 0)
     return 1;
   return 0;
 }
 
 /**
- * execute_cd_command - executes the cd command
- * @args: An array of arguments for the cd command
+ * change_dir - executes the cd command
+ * @args: array of arguments for the cd command
+ * Return: 0 Success, 1 on failure (For Old Pwd Always 0 Case No Old PWD)
  */
-void execute_cd_command(char **args) {
-  if (args[1] == NULL) {
-    fprintf(stderr, "Expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("Error");
-    }
-  }
+int change_dir(char **args)
+{
+	int store = -1;
+	char working_dir[PATH_MAX];
+
+	if (args[1] == NULL)
+		store = chdir(getenv("HOME"));
+	else if (strcmp_(cmd[1], "-") == 0)
+	{
+		store = chdir(getenv("OLDPWD"));
+	}
+	else
+		store = chdir(args[1]);
+
+	if (store == -1)
+	{
+		perror("hsh");
+		return (-1);
+	}
+	else if (store != -1)
+	{
+		getcwd(working_dir, sizeof(cwd));
+		setenv("OLDPWD", getenv("PWD"), 1);
+		setenv("PWD", working_dir, 1);
+	}
+	return (0);
 }
