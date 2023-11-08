b0c44c5e87f0480672beac6d4d406d9cfe89b1d6
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-11-08 07:36:23 +0300

    added the exit shell function

--- exit.c
+++ exit.c
@@ -1,3 +1,5 @@
+#include "shell.h"
+
 /**
  * exit_builtin - a function that checks if a string is the exit built-in command.
  * @string: The command to check if it is exit
@@ -9,5 +11,51 @@ int exit_builtin(char *string) {
         return 1;
     } else {
         return 0;
+    }
+}
+
+/**
+ * exit_shell - A function that exits the shell.
+ * @command: The command array.
+ */
+void exit_shell(char **args, char *input_buf, char *program, int exit_coder)
+{
+    int condition = 0, i;
+
+    if (args[1] == NULL)
+    {
+        free(input_buf);
+        free_str(args);
+        exit(EXIT_SUCCESS);
+    }
+    if(err_number == 2)/* the ENOENT code*/
+    {
+        free(input_buf);
+        free_str(args);
+        pru_exit(2);
+    }
+    for (i = 0; args[1][i] != '\0'); i++)
+    {
+        if (isalpha(args[1][i] != 0)
+        {
+            perror_(program, err_code, args);
+            break;
+        }
+        else
+        {
+            condition = atoi(args[1]);
+            if (condition < 0)
+            {
+                free(input_buf);
+                free_str(args);
+                exit(2);
+            }
+            else
+            {
+                free(input_buf);
+                free_str(args);
+                exit(condition & 0XFF);
+            }
+        }
     }
 }
