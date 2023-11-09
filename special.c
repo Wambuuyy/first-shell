71a5338dcce9c817167677255912c91ca0960505
Prudence Wambui <wambuiprudy@gmail.com>
2023-11-09 12:14:58 +0300

    created the strcat function

--- special.c
+++ special.c
@@ -56,3 +56,42 @@ int strcmp_(const char *string1, const char *string2)
 	}
 	return (*string1 - *string2);
 }
+
+/**
+ * strcat_ - function that concatenates two strings
+ * @string1 - the destination string
+ * @string2 - second string the source 
+ * Return: a pointer to the resulting string or null on failure
+ */
+char *strcat_(char *string1, char *string2)
+{
+	char *dest = string1, *result;
+	size_t i = 0, str1_len = 0, str2_len = 0;
+
+	while (*string1 != '\0')
+	{
+		string1++;
+		str1_len++;
+	}
+	while (string2[str2_len] != '\0')
+	{
+		str2_len++;
+	}
+	result = malloc(str1_len + str2_len + 1);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	while (i < str1_len)
+	{
+		result[i] = dest[i];
+		i++;
+	}
+	while (i < str1_len + str2_len)
+	{
+		result[i] = string2[i - str1_len];
+		i++;
+	}
+	result[str1_len + str2_len] = '\0';
+	return (result);
+}
